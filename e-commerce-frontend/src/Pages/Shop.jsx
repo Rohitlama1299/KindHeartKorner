248940761c25bb733cb36d1d0318bd1bb3a8b9e1
Je2303 <116121987+JesusSanchez21@users.noreply.github.com>
2024-03-10 21:47:39 +0000

    fix fronted

--- e-commerce-frontend/src/Pages/Shop.jsx
+++ e-commerce-frontend/src/Pages/Shop.jsx
@@ -1,25 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Hero from '../Components/Hero/Hero'
-import Popular from '../Components/Popular/Popular'
 
 
 const Shop = () => {
 
-  const [popular, setPopular] = useState([]);
-  const [newcollection, setNewCollection] = useState([]);
-
-  const fetchInfo = () => { 
-    fetch('http://localhost:4000/popularinwomen') 
-            .then((res) => res.json()) 
-            .then((data) => setPopular(data))
-    fetch('http://localhost:4000/newcollections') 
-            .then((res) => res.json()) 
-            .then((data) => setNewCollection(data))
-    }
-
-    useEffect(() => {
-      fetchInfo();
-    }, [])
 
 
   return (
@@ -27,7 +11,6 @@ const Shop = () => {
       <Hero/>
       <br />
       <br />
-      <Popular data={popular}/>
     </div>
   )
 }
